59f091f4c962cbfbc7c5ed7b90872267ab34902d
dedyfuadi92 <dedyfuadi92@gmail.com>
2023-09-01 15:27:24 +0800

    role and permission

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,9 +17,17 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $user1 = User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@test.com',
+        ]);
+        $user2 = User::factory()->create([
+            'name' => 'Editor',
+            'email' => 'editor@test.com',
+        ]);
+        $role = Role::create(['name' => 'admin']);
+        $user1->assignRole($role);
+        $role = Role::create(['name' => 'editor']);
+        $user1->assignRole($role);
     }
 }
